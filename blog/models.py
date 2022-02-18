947a7b41ce2b9740b45eca49cc248e30cb87bf1a
viniciuspossato <viniciuspossato12@gmail.com>
2022-02-18 11:57:59 -0300

    Criação do Portal Post

---
+++ blog/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    STATUS = (
+        ('rascunho','Rascunho'),
+        ('publicar','Publicar'),
+    )
+
+    # O que existe em nossa postagem? Título, Autor, Slug e o Corpo/Conteúdo...
+
+    titulo = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    autor = models.ForeignKey(User,
+                              on_delete=models.CASCADE)
+    conteudo = models.TextField() # Campo com muito conteúdo
+
+    publicado = models.DateField(default = timezone.now()) # Data de publicação: Agora!
+    criado = models.DateField(auto_now_add = True) # Apenas na criação ele será setado
+    alterado = models.DateField(auto_now=True) # Sempre que for atualizado, a hora muda
+
+    status = models.CharField(choices=STATUS,
+                              max_length=10,
+                              default='rascunho')
+
+    class Meta:
+        ordering = ('-publicado',) # ordenando as postagens de acordo com a data de pubkicação
+
+    def __str__(self): # Dando às postagens os seus títulos
+        return '{} - {}'.format(self.titulo,self.autor)
+
+# Create your models here.
+
