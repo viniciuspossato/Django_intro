9f605dded7267e2a6736c0d8068b5d21526517b4
viniciuspossato <viniciuspossato12@gmail.com>
2022-02-19 14:50:00 -0300

    Managers e querysets

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post) # Registrando a minha aplicação
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    # Lista de exibição
+    list_display = ('titulo','autor','conteudo','publicado','status')
+
+    # Aba de pesquisa de posts
+    search_fields=('titulo','conteudo')
+
+    # Preenchimento automático do slug de acordo com o titulo
+    prepopulated_fields = {'slug' : ('titulo',)}
+
+    # Inserindo aba lateral de filtros
+    list_filter = ('status','criado','publicado','autor')
+
+    # Incluindo aba de pesquisa detalhada de data
+    date_hierarchy = 'publicado'
+
+    # IDs dos autores (funciona na hora de criar posts)
+    raw_id_fields = ('autor',)
+
 # Register your models here.
